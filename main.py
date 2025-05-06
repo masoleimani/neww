4d6d4d3e5d063d45ff16503162266687e5cac1c2
masoleimani <136060808+masoleimani@users.noreply.github.com>
2025-05-06 15:30:22 +0330

    Update main.py

--- main.py
+++ main.py
@@ -33,5 +33,5 @@ elif choice == '3':
 elif choice == '4':
     print("نتیجه: ", divide(num1, num2))
 else:
-    print("انتخاب نامعتبر است")
-     print("انتخاب نامعتبر است")
+    print("انتخاب نامعتبر ")
+  
